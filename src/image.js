1e6aacaa2320aa37042bbb605d3f5cc2d3d9fd5b
foamy-latte <35882542+foamy-latte@users.noreply.github.com>
2019-01-14 00:17:30 +0900

    Fix: Image manipulation

--- src/image.js
+++ src/image.js
@@ -1,38 +1,66 @@
+// image.js
+/*
+ * Image manipulation.
+ */
 "use strict";
-var Jimp = require("jimp");
+
+const Jimp = require("jimp");
+
 /**
- * @param {Buffer[]} images An array of image buffers
+ * Composes multiple images one offset by another into a single PNG image.
+ * Transparency is preserved.
+ * @param {Buffer[]} images An array of image buffers.
+ * @returns	{external:Promise} A promise with a buffer containing an image output.
  */
 function composeImage(images){
-	// Stack all images into one png
-	let imagePromises = images.map(function(element){
-		return new Promise(function(resolve, reject){
-			Jimp.read(element).then(image => {
-				resolve(image);
+	return new Promise(function(resolve, reject){
+		// Create array of promises containing images
+		let imagePromises = images.map(function(element){
+			return new Promise(function(resolve, reject){
+				Jimp.read(element).then(function(err, image){
+					if (err){
+						reject(err);
+					} else {
+						resolve(image);
+					}
+				});
 			});
 		});
-	});
-	Promise.all(imagePromises).then(function(imageArray){
-		let maxWidth = Math.max(imageArray.map(function(image){
-			return image.bitmap.width;
-		}));
-		console.log(maxWidth);
 
-		let totalHeight = 0;
-		for (let image in imageArray){
-			totalHeight += image.bitmap.height;
-		}
-		console.log(totalHeight);
+		// Create and work on array of images
+		Promise.all(imagePromises).then(function(imageArray){
+			// Calculate maximum width among the images
+			let maxWidth = Math.max(imageArray.map(function(image){
+				return image.bitmap.width;
+			}));
 
-		// Generate new JIMP instance
-		let jimpImage = new Jimp(maxWidth, totalHeight, 0, (err, canvas) => {
-			let currentOffset = 0;
-			for (image in imageArray){
-				canvas.composite(image, 0, currentOffset, {
-					mode: Jimp.BLEND_OVERLAY
-				});
-				currentOffset += image.bitmap.height;
+			// Calculate sum of image heights
+			let totalHeight = 0;
+			for (let image in imageArray){
+				totalHeight += image.bitmap.height;
 			}
+
+			// Generate new JIMP instance
+			let jimpImage = new Jimp(maxWidth, totalHeight, 0, (err, canvas) => {
+				if (err){
+					reject(err);
+				} else {
+					let currentOffset = 0;
+					for (let image in imageArray){
+						canvas.composite(image, 0, currentOffset, {
+							mode: Jimp.BLEND_OVERLAY
+						});
+						currentOffset += image.bitmap.height;
+					}
+					resolve(jimpImage.getBufferAsync(Jimp.MIME_PNG));
+				}
+			});
+		}, function(err){
+			reject(err);
 		});
 	});
-};
+}
+
+module.exports = {
+	composeImage
+}
